0a610661815ade5e1c0c79d3c3411da9525b1d2a
Jinyue Song <jinyuesong88@gmail.com>
2015-11-06 10:10:56 -0500

    begin

---
+++ main.cpp
@@ -0,0 +1,26 @@
+//
+//  main.cpp
+//  Terrain
+//
+//  Created by Jinyue Song on 2015-11-06.
+//  Copyright © 2015 Jinyue Song. All rights reserved.
+//
+
+#include <stdlib.h>
+#include <stdio.h>
+#include <math.h>
+
+#ifdef __APPLE__
+#   include <OpenGL/gl.h>
+#   include <OpenGL/glu.h>
+#   include <GLUT/glut.h>
+#else
+#   include <GL/gl.h>
+#   include <GL/glu.h>
+#   include <GL/freeglut.h>
+#endif
+
+int main(int argc, char * argv[]) {
+    
+    return 0;
+}
