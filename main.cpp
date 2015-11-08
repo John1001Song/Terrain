4949d6c4e5d254aae226c4e4e49ded05f2243a60
Jinyue Song <jinyuesong88@gmail.com>
2015-11-08 18:07:53 -0500

    draw and height

--- main.cpp
+++ main.cpp
@@ -53,8 +53,140 @@ void initMap(){
  this pseudocode is from http://www.lighthouse3d.com/opengl/terrain/index.php?circles to help with coding
 */
 
-void drawCir(vertex3D::vertex3D){
-    
+int circSize = 20;
+int disp = 4;
+void drawCir(){
+    for (int indxRadm = 0; indxRadm < 100; indxRadm++) {
+        int centerX = randPos[indxRadm][0];
+        int centerZ = randPos[indxRadm][1];
+        
+        //determine the circle is out of the map boundary or not
+        //have a clear circle boundary and within the circle, catch all the pixel and calculate the height of point
+        //in half circle, there is a triangle with 90 degree for three vertces on the circle
+        //height is a and x/a = a/(longSide-x)
+        
+        
+        for (int x = centerX - circSize/2; x < centerX + circSize/2; x++) {
+            int a = sqrtf(x * (circSize-x));
+            for (int z = centerZ - a; z < centerZ + a; z++) {
+                if (300 > x && x >= 0) {
+                    if (300 > z && z >= 0) {
+                        map[x][z] += disp/2 + cosf(sqrtf( (x-centerX)*(x-centerX)+(z-centerZ)*(z-centerZ)) * 3.14) * disp/2;
+
+                    }
+                }
+            }
+        }
+    }
+        
+        //following is the code to determine if (x, z) is in map 300*300
+        //but it is too redundent with if else statement
+        //just ignore
+        
+        /*
+        
+        
+        
+        if (300-circSize/2 >= centerX && centerX >= 0+circSize/2) {
+            if (300-circSize/2 >= centerZ && centerZ >= 0+circSize/2) {
+                
+                for (int x = centerX - circSize/2; x < centerX + circSize/2; x++) {
+                    int a = sqrtf(x * (circSize-x));
+                    for (int z = centerZ - a; z < centerZ + a; z++) {
+                        map[x][z] += disp/2 + cosf(sqrtf( (x-centerX)*(x-centerX)+(z-centerZ)*(z-centerZ)) * 3.14) * disp/2;
+                    }
+                    
+                }
+            }else{
+                if (centerZ - circSize/2 < 0) {
+                    for (int x = centerX - circSize/2; x < centerX + circSize/2; x++) {
+                        int a = sqrtf(x * (circSize-x));
+                        for (int z = 0; z < centerZ + a; z++) {
+                            map[x][z] += disp/2 + cosf(sqrtf( (x-centerX)*(x-centerX)+(z-centerZ)*(z-centerZ)) * 3.14) * disp/2;
+                        }
+                    }
+                }
+                if (300 - centerZ < circSize/2) {
+                    for (int x = centerX - circSize/2; x < centerX + circSize/2; x++) {
+                        int a = sqrtf(x * (circSize-x));
+                        for (int z = centerZ - a; z < 300; z++) {
+                            map[x][z] += disp/2 + cosf(sqrtf( (x-centerX)*(x-centerX)+(z-centerZ)*(z-centerZ)) * 3.14) * disp/2;
+                        }
+                        
+                    }
+                }
+            }
+        }
+        else {if (centerX < circSize/2){
+            if (300-circSize/2 >= centerZ && centerZ >= 0+circSize/2) {
+                
+                for (int x = 0; x < centerX + circSize/2; x++) {
+                    int a = sqrtf(x * (circSize-x));
+                    for (int z = centerZ - a; z < centerZ + a; z++) {
+                        map[x][z] += disp/2 + cosf(sqrtf( (x-centerX)*(x-centerX)+(z-centerZ)*(z-centerZ)) * 3.14) * disp/2;
+                    }
+                    
+                }
+            }else{
+                if (centerZ - circSize/2 < 0) {
+                    for (int x = 0; x < centerX + circSize/2; x++) {
+                        int a = sqrtf(x * (circSize-x));
+                        for (int z = 0; z < centerZ + a; z++) {
+                            map[x][z] += disp/2 + cosf(sqrtf( (x-centerX)*(x-centerX)+(z-centerZ)*(z-centerZ)) * 3.14) * disp/2;
+                        }
+                    }
+                }
+                if (300 - centerZ < circSize/2) {
+                    for (int x = 0; x < centerX + circSize/2; x++) {
+                        int a = sqrtf(x * (circSize-x));
+                        for (int z = centerZ - a; z < 300; z++) {
+                            map[x][z] += disp/2 + cosf(sqrtf( (x-centerX)*(x-centerX)+(z-centerZ)*(z-centerZ)) * 3.14) * disp/2;
+                        }
+                        
+                    }
+                }
+            }
+
+        }
+        
+            }
+        if (300 - centerX < circSize/2) {
+            if (300-circSize/2 >= centerZ && centerZ >= 0+circSize/2) {
+                
+                for (int x = centerX - circSize/2; x < 300; x++) {
+                    int a = sqrtf(x * (circSize-x));
+                    for (int z = centerZ - a; z < centerZ + a; z++) {
+                        map[x][z] += disp/2 + cosf(sqrtf( (x-centerX)*(x-centerX)+(z-centerZ)*(z-centerZ)) * 3.14) * disp/2;
+                    }
+                    
+                }
+            }else{
+                if (centerZ - circSize/2 < 0) {
+                    for (int x = centerX - circSize/2; x < 300; x++) {
+                        int a = sqrtf(x * (circSize-x));
+                        for (int z = 0; z < centerZ + a; z++) {
+                            map[x][z] += disp/2 + cosf(sqrtf( (x-centerX)*(x-centerX)+(z-centerZ)*(z-centerZ)) * 3.14) * disp/2;
+                        }
+                    }
+                }
+                if (300 - centerZ < circSize/2) {
+                    for (int x = centerX - circSize/2; x < 300; x++) {
+                        int a = sqrtf(x * (circSize-x));
+                        for (int z = centerZ - a; z < 300; z++) {
+                            map[x][z] += disp/2 + cosf(sqrtf( (x-centerX)*(x-centerX)+(z-centerZ)*(z-centerZ)) * 3.14) * disp/2;
+                        }
+                        
+                    }
+                }
+            }
+
+        }
+        
+        
+    }
+}
+         
+         */
 }
 
 
